e5b8563ca0a252a90cbc265dfeae7701c075d265
Timothy Kuijper <t.kuijper12@gmail.com>
2022-11-24 12:43:22 +0100

    added js and html file

---
+++ les1/app.js
@@ -0,0 +1,26 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        let appname = "gaat het goed?";
+        let version = 1.6;
+        let versiondate = "24/11/2022";
+        let author = "Timothy Kuijper";
+        let copyright = "DEXXION trademark '2022'";
+        let distribution = "DEXXION inc.";
+        let darkmode = true;
+        console.log(appname);
+        console.log(version);
+        console.log(versiondate);
+        console.log(author);
+        console.log(copyright);
+        console.log(distribution);
+        console.log(darkmode);
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+
