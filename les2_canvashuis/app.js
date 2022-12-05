4badec58273b978f6974c9fa0ac9b9cdb2fdbc6a
Timothy Kuijper <t.kuijper12@gmail.com>
2022-12-05 10:29:51 +0100

    added multiple files + some updates

---
+++ les2_canvashuis/app.js
@@ -0,0 +1,48 @@
+class App
+{
+    runApplication()
+    {
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        console.log(canvas);
+        g.beginPath()
+        g.fillStyle = "brown"
+        g.moveTo(225,100);
+        g.lineTo(300,160);
+        g.lineTo(300,160);
+        g.lineTo(300,300);
+        g.lineTo(160,300);
+        g.lineTo(160,160);
+        g.closePath();
+        g.stroke();
+        g.fill();
+        g.beginPath();
+        g.moveTo(210,200)
+        g.lineTo(210,175)
+        g.lineTo(185,175)
+        g.lineTo(185,200)
+        g.closePath();
+        g.stroke();
+        g.beginPath();
+        g.moveTo(197.5,175);
+        g.lineTo(197.5,200);
+        g.moveTo(185,187.5)
+        g.lineTo(210,187.5)
+        g.closePath();
+        g.stroke();
+        g.beginPath();
+        g.moveTo(275,140);
+        g.lineTo(275,100);
+        g.lineTo(255,100);
+        g.lineTo(255,125)
+        g.closePath();
+        g.fill();
+
+
+  
+    }
+}
+let app = new App();
+app.runApplication();
+
+
---
+++ les3_ifelse/app.js
@@ -0,0 +1,56 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+        let appname = "gaat het goed?";
+        let version = 1.6;
+        let versiondate = "24/11/2022";
+        let author = "Timothy Kuijper";
+        let copyright = "DEXXION trademark '2022'";
+        let distribution = "DEXXION inc.";
+        let darkmode = true;
+        console.log(appname);
+        console.log(version);
+        console.log(versiondate);
+        console.log(author);
+        console.log(copyright);
+        console.log(distribution);
+        console.log(darkmode);
+        let title = document.getElementById("newstitle");
+        let RG = Math.random();
+        
+        if (RG == 0)
+        {
+            title.style.backgroundColor ="#410ddb";
+        }
+
+        if (RG < 0.2)
+        {
+            title.style.backgroundColor ="#b817d1";
+        }
+
+        if (RG >= 0.2 && RG <= 0.6 )
+        {
+            title.style.backgroundColor ="#f50707";
+        }
+
+        if (RG > 0.6)
+        {
+            title.style.backgroundColor ="#07f5c9";
+        }
+
+    
+        //.gamenews headline{
+
+        //}
+        //.gamenews{
+
+        //}
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+
