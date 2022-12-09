c5e08a7e615085c3a7232108a56730e903fe697a
Timothy Kuijper <t.kuijper12@gmail.com>
2022-12-09 10:16:46 +0100

    finished

--- les5_loops/canvas/app.js
+++ les5_loops/canvas/app.js
@@ -3,28 +3,28 @@ class App
     runApplication()
     { 
         console.log("hello world");
-
-        this.tekenCircle()
-        {
-            let a = (int)(Math.random() * 301);
-            let b = (int)(Math.random() * 301);
-            let canvas = document.getElementById("canvas");
-            let g = canvas.getContext("2d");
-            console.log(canvas);
-            g.beginPath();
-            g.fillstyle = "yellow";
-            g.arc(100,100,20,0,math.PI*2);
-            g.strok();
-            g.fill();
-            g.closepath();
-
-        }
-
-        for(let i = 0; i < 200; 1++);
-        {
-            tekenCircle();        
+        for (let i = 0; i < 201; i++) {
+            this.tekenCircle();
+            
         }
     }
+
+    tekenCircle()
+    {
+        let a = (Math.random() * 1601);
+        let b = (Math.random() * 601);
+        let canvas = document.getElementById("canvas");
+        let g = canvas.getContext("2d");
+        console.log(canvas);
+        g.beginPath();
+        g.fillstyle = "yellow"
+        g.arc(a,b,20,0,Math.PI*2);
+        g.stroke();
+        g.fill();
+        g.closePath();
+    }
+    
+    
 }
 
 
