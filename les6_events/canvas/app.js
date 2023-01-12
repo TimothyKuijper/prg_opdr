47fee5f3bf290aa286b6d4dd23421be62c495e4b
Timothy Kuijper <t.kuijper12@gmail.com>
2023-01-12 12:34:35 +0100

    added assignments

--- les6_events/canvas/app.js
+++ les6_events/canvas/app.js
@@ -2,7 +2,28 @@ class App
 {
     runApplication()
     {
+        let myCanvas = document.getElementById("canvasID");
+        let g = myCanvas.getContext("2d");
         
+
+        myCanvas.addEventListener("mousemove", (evt)=>{
+            console.log("mouse move");
+
+        })
+
+        g.fillStyle = "green";
+        g.rect(100, 100, 30, 30);
+        g.stroke();
+        g.fill();
+
+    }
+
+    getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
     }
 }
 
